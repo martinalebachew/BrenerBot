aae7afa2aecbae2db7e09346999d854823201f21
Martin Alebachew <martinalebachew@gmail.com>
2023-04-11 02:28:37 +0300

    Sped up database operations
    - Sped up MongoDB operations by creating a document for the whole specified directory, instead of creating a document for each file. Benchmarking on my local machine showed improvement in upload time of 59.8s -> 1.4s!
    - Download methods will be updated in future commits.

--- src/mongodb-api/client.ts
+++ src/mongodb-api/client.ts
@@ -5,8 +5,6 @@ import { MongoClient, Db, Collection, ObjectId } from "mongodb";
 import type { WithId, Document } from "mongodb";
 import { rmSync, writeFileSync, mkdirSync, readdirSync, statSync, readFileSync } from "fs";
 
-const DATABASE = "whatsapp-auth";
-
 interface IWrappedData extends WithId<Document> {
     _id: ObjectId,
     filename: string,
@@ -26,12 +24,10 @@ class WrappedData implements IWrappedData {
 
 export class Client {
     private connection: MongoClient;
-    private database: Db;
 
     constructor(username: string, password: string, endpoint: string) {
         const uri = `mongodb+srv://${username}:${password}@${endpoint}/?retryWrites=true&w=majority`;
         this.connection = new MongoClient(uri);
-        this.database = this.connection.db(DATABASE);
     }
 
     public async closeConnection() {
@@ -41,31 +37,37 @@ export class Client {
     public async uploadDirectory(folder: string) {
         console.log("\nUploading authentication files...");
 
-        const collection = this.database.collection(folder);
-        await this.uploadDirectoryImpl(collection, folder);
+        const database = this.connection.db("persistent-storage");
+        const collection = database.collection(folder);
+        const wrappedArray = await this.fetchDirectory(folder);
+        await collection.insertOne({ wrappedArray });
+
         console.log("Successfully uploaded authentication files.\n");
     }
 
-    private async uploadDirectoryImpl(collection: Collection, folder: string) {
+    private async fetchDirectory(folder: string) {
+        const wrappedArray: WrappedData[] = [];
         const files = readdirSync("./" + folder);
         for (const filename of files) {
             const relativePath = folder + "/" + filename;
-            if (!statSync(relativePath).isDirectory()) await this.uploadFile(collection, relativePath);
-            else await this.uploadDirectoryImpl(collection, relativePath);
+            if (!statSync(relativePath).isDirectory()) wrappedArray.push(await this.fetchFile(relativePath));
+            else wrappedArray.concat(await this.fetchDirectory(relativePath));
         }
 
         if (files.length === 0) {  // Empty directory
             const relativePath = folder + "/" + "@";
-            await this.uploadFile(collection, relativePath, true);
+            wrappedArray.push(await this.fetchFile(relativePath, true));
         }
+
+        return wrappedArray;
     }
 
-    public async uploadFile(collection: Collection, relativePath: string, empty = false) {
+    public async fetchFile(relativePath: string, empty = false) {
         const data = empty ? "" : readFileSync(relativePath, { encoding: "base64" });  // Read file contents and encode as base64
         const relativePathNoFolder = relativePath.split("/").slice(1).join("/");
         const wrapped = new WrappedData(relativePathNoFolder, data);
-        await collection.insertOne(wrapped);
-        console.log("* Uploaded: ", relativePath);
+        console.log("* Fetched: ", relativePath);
+        return wrapped;
     }
 
     public downloadFile(folder: string, wrapped: WrappedData) {
@@ -86,7 +88,8 @@ export class Client {
         console.log("\nDownloading authentication files...");
         rmSync(folder, { recursive: true, force: true });
 
-        const collection = this.database.collection(folder);
+        const database = this.connection.db("persistent-storage");
+        const collection = database.collection(folder);
         const documents = await collection.find({ });
         await documents.forEach((document) => {
             this.downloadFile(folder, document as WrappedData);
