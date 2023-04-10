9344ac4eaaa2dde0dc64784be1155a76c7f2e220
Martin Alebachew <martinalebachew@gmail.com>
2023-04-10 11:03:26 +0300

    Changed MongoDB directory
    - Changed MongoDB WhatsApp credentials database and collection.

--- src/mongodb-api/client.ts
+++ src/mongodb-api/client.ts
@@ -1,67 +1,67 @@
 // client.ts
 // (C) Martin Alebachew, 2023
 
-import { MongoClient, Db, Collection } from "mongodb"
-import { rmSync, writeFileSync, mkdirSync } from "fs"
-import { join } from "path"
+import { MongoClient, Db, Collection } from "mongodb";
+import { rmSync, writeFileSync, mkdirSync } from "fs";
+import { join } from "path";
 
-const DATABASE = "api-auth"
-const COLLECTION = "whatsapp"
+const DATABASE = "whatsapp-api";
+const COLLECTION = ".wweb_auth";
 
 class WrappedData {
-    public filename: string
-    public data: any
+    public filename: string;
+    public data: any;
 
     constructor(filename: string, data: any) {
-        this.filename = filename
-        this.data = data
+        this.filename = filename;
+        this.data = data;
     }
 }
 
 export class Client {
-    private connection: MongoClient
-    private database: Db
-    private collection: Collection
+    private connection: MongoClient;
+    private database: Db;
+    private collection: Collection;
 
     constructor(username: string, password: string, endpoint: string) {
-        const uri = `mongodb+srv://${username}:${password}@${endpoint}/?retryWrites=true&w=majority`
-        this.connection = new MongoClient(uri)
-        this.database = this.connection.db(DATABASE)
-        this.collection = this.database.collection(COLLECTION)
+        const uri = `mongodb+srv://${username}:${password}@${endpoint}/?retryWrites=true&w=majority`;
+        this.connection = new MongoClient(uri);
+        this.database = this.connection.db(DATABASE);
+        this.collection = this.database.collection(COLLECTION);
     }
 
     public async closeConnection() {
-        await this.connection.close()
+        await this.connection.close();
     }
 
     public async save(filename: string, data: any) {
-        const wrapped = new WrappedData(filename, data)
-        await this.collection.insertOne(wrapped)
+        const wrapped = new WrappedData(filename, data);
+        await this.collection.insertOne(wrapped);
     }
 
     public async update(filename: string, data: any) {
-        await this.remove(filename)
-        await this.save(filename, data)
+        await this.remove(filename);
+        await this.save(filename, data);
     }
 
     public async remove(filename: string) {
-        const query = { filename: filename }
-        await this.collection.deleteOne(query)
+        const query = { filename: filename };
+        await this.collection.deleteOne(query);
     }
 
     public async downloadAll(folder: string) {
-        console.log("\nDownloading authentication files...")
-        rmSync(folder, { recursive: true, force: true })
-        mkdirSync(folder)
-        const documents = await this.collection.find({ })
+        console.log("\nDownloading authentication files...");
+        rmSync(folder, { recursive: true, force: true });
+        mkdirSync(folder);
+        const documents = await this.collection.find({ });
         await documents.forEach((document) => {
-            const filename = document.filename
-            delete document.filename
+            const filename = document.filename;
+            delete document.filename;
 
-            writeFileSync(join(folder, filename), document.data)
-            console.log("* Downloaded: ", filename)
-        })
+            writeFileSync(join(folder, filename), document.data);
+            console.log("* Downloaded: ", filename);
+        });
 
-        console.log("Successfully downloaded authentication files.\n")
+        console.log("Successfully downloaded authentication files.\n");
     }
 }
